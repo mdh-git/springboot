99dd6686d20e8d21be13cfb6660bd76d91b6e9ab
madonghao <752228071@qq.com>
2018-07-24 09:14:52 +0800

    Init

---
+++ src/main/java/com/mdh/MdhServiceApplication.java
@@ -0,0 +1,12 @@
+package com.mdh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdhServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MdhServiceApplication.class, args);
+	}
+}
---
+++ src/main/java/com/mdh/controller/TestController.java
@@ -0,0 +1,23 @@
+package com.mdh.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by mdh on 2018/7/23.
+ */
+@RestController
+public class TestController {
+    final Logger logger = LoggerFactory.getLogger(TestController.class);
+
+    @RequestMapping("/hello")
+    public String hello(){
+        logger.info("dev");
+        logger.debug("debug");
+        logger.error("error");
+        logger.warn("warn");
+        return "Hello Spring Boot !!!";
+    }
+}
